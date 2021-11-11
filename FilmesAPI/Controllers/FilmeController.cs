e129b667c1f19dcabb9e58a0561c5d883bc674b6
Fabio Henrique Antonio <fabio.henrique100@hotmail.com>
2021-11-11 08:55:18 -0300

    Recebendo os dados do Filme

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -3,13 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FilmesAPI.Models;
 
 namespace FilmesAPI.Controllers
 {
 
     [ApiController]
-    [Route("controller")]
-    public class FilmeController
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
     {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
     }
 }
